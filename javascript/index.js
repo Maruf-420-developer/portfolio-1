6c7b5f37c3073f7cd241931ac7227ea0681c5cad
Maruf-420-developer <marufkhandurjoy9359@gmail.com>
2024-10-17 14:13:35 +0600

    nav_link

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,15 @@
 const nav_menu = document.querySelector(".menu");
 const nav_header = document.querySelector("header");
+const nav_links = document.getElementsByClassName("nav_link");
 
 const toggleNavbar = () => {
   nav_header.classList.toggle("active");
 };
 
 nav_menu.addEventListener("click", () => toggleNavbar());
+
+for (const navLink of nav_links) {
+  navLink.addEventListener("click", () => {
+    nav_header.classList.toggle("active");
+  });
+}
